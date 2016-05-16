30ad75a0b26feb89de51fb4850401e657f1e960a
quang <ga21ru21@gmail.com>
2016-05-16 17:30:14 +0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
-gulp.task('default', () => {
+gulp.task('default', function() {
     return gulp.src('./test/test-server.js', {read: false})
         // gulp-mocha needs filepaths so you can't have any plugins before it
         .pipe(mocha({reporter: 'nyan'}));
