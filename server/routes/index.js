0117ba22c15c08c497dee8c7aadb52fe0bcebb8c
quang <ga21ru21@gmail.com>
2016-05-17 17:23:14 +0700

    123

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Blob = require('../models/blob');
 
 router.get('/', function(req, res, next) {
-  res.send('Hello, QuangHD!');
+  res.send('Hello, 123!');
 });
 
 // *** api routes *** //
