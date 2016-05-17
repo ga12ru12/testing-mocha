20174ce8ffddc49e26159f203f420dcc934a864f
quang <ga21ru21@gmail.com>
2016-05-17 09:45:00 +0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,11 @@ const mocha = require('gulp-mocha');
 gulp.task('default', function() {
     return gulp.src('./test/test-server.js', {read: false})
         // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({reporter: 'nyan'}));
+        .pipe(mocha({reporter: 'nyan'}))
+        .once('error', function(){
+            process.exit(1);
+        })
+        .once('end', function(){
+            process.exit();
+        });
 });
